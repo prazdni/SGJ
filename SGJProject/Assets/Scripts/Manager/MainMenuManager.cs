5ea2998167cd0590ad2ef9d3f87fe93a87d6938b
Danil <danilstasilevich@yandex.ru>
2021-04-01 19:53:02 +0700

    Добавил меню

---
+++ SGJProject/Assets/Scripts/Manager/MainMenuManager.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+namespace Manager
+{
+    public class MainMenuManager : MonoBehaviour
+    {
+        [SerializeField] private Button _startButton;
+        [SerializeField] private Button _musicButton;
+        [SerializeField] private Button _exitButton;
+
+        private bool _shouldSound;
+        
+        private void Awake()
+        {
+            _startButton.onClick.AddListener(OnStartClick);
+            _musicButton.onClick.AddListener(OnMusicClick);
+            _exitButton.onClick.AddListener(OnExitClick);
+
+            _shouldSound = true;
+        }
+
+        private void OnStartClick()
+        {
+            SceneManager.LoadScene(1);
+        }
+
+        private void OnMusicClick()
+        {
+            _shouldSound = !_shouldSound;
+            AudioManager.Instance.AdjustMusicVolume(_shouldSound);
+        }
+
+        private void OnExitClick()
+        {
+#if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+#endif
+            Application.Quit();
+        }
+    }
+}
